3e6f158b6842de5a8100978ff4481df55e6615d3
marty-suzuki <s1180183@gmail.com>
2018-06-29 14:13:05 +0900

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Degu",
-            targets: ["Degu"]),
+            targets: ["Degu", "RuntimeHandler"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
